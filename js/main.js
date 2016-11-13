55c1b917ab6ad276ff6e2e3906449457c48a2d5f
smo957 <ohsungmin95@gmail.com>
2016-11-13 16:11:52 -0500

    add new files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
 /**
  * Created by sungmin on 9/20/16.
  */
+
+var allData = [];
+
+// Variable for the visualization instance
+var aggregateChart;
+
+// Start application by loading the data
+loadData();
+
+
+function loadData() {
+
+}
+
+function createVis() {
+
+    // Instantiate Visualization
+    aggregateChart = new StationMap("station-map", allData, [42.360082, -71.058880]);
+
+}
---
+++ js/template.js
@@ -0,0 +1,54 @@
+/**
+ * Created by sungmin on 11/13/16.
+ */
+
+/*
+ *  AggregateChart - Object constructor function
+ *  @param _parentElement   -- HTML element in which to draw the visualization
+ *  @param _data            -- ???
+ */
+
+AggregateChart = function(_parentElement, _data) {
+
+    this.parentElement = _parentElement;
+    this.data = _data;
+
+    this.initVis();
+}
+
+
+/*
+ *  Initialize visualization (static content, e.g. SVG area or axes)
+ */
+
+AggregateChart.prototype.initVis = function() {
+    var vis = this;
+
+    vis.wrangleData();
+}
+
+
+/*
+ *  Data wrangling
+ */
+
+AggregateChart.prototype.wrangleData = function() {
+    var vis = this;
+
+    // Currently no data wrangling/filtering needed
+    // vis.displayData = vis.data;
+
+    // Update the visualization
+    vis.updateVis();
+
+}
+
+
+/*
+ *  The drawing function
+ */
+
+AggregateChart.prototype.updateVis = function() {
+    var vis = this;
+
+}
